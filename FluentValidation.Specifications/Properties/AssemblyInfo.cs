2c20db2678b40f9411a637c86033202bbabb6ea6
Simon Rigby <simon.rigby@levelnis.co.uk>
2012-11-13 22:34:25 +0000

    Added nuget package

--- FluentValidation.Specifications/Properties/AssemblyInfo.cs
+++ FluentValidation.Specifications/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FluentValidation.Specifications")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An NBehave Specification library for simplified testing of FluentValidation validators.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyCompany("Levelnis Ltd.")]
 [assembly: AssemblyProduct("FluentValidation.Specifications")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2012")]
+[assembly: AssemblyCopyright("Copyright © Levelnis Ltd. 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
